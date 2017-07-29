6dbae01f5f69135cd7d70cb6cc9af25ad2900c14
Casey Primozic <me@ameo.link>
2017-07-29 14:01:52 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MySQL Log Visualization
+
+A utility for parsing the log files of a MySQL server and using them to create a web-based visualization.  Created for the 2017 Freak What You Feel Hackathon.  Built using Rust and WebAssembly.
---
+++ backend/src/main.rs
@@ -0,0 +1,6 @@
+//! MySQL Server Log File Visualization Backend.  Provides the log tailing functionality as well as hosting the
+//! Minutiae server to power the visualization and transmit events to the clients.
+
+fn main() {
+    println!("Hello, world!");
+}
